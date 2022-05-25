db7d43fc10f5aaea8e381dea0bbac21d83070116
kranthi <kranthi@aveshasystems.com>
2022-05-26 02:32:19 +0530

    modified to validate Charts

--- kind/kind.sh
+++ kind/kind.sh
@@ -14,7 +14,11 @@
 #	See the License for the specific language governing permissions and
 #	limitations under the License.
 
-ENV=kind.env
+
+if [ "$OSS_CHARTS" -ne "charts" ] || [ "$OSS_CHARTS" -ne "dev-charts" ]; then
+    ENV=kind.env
+fi
+
 CLEAN=false
 VERBOSE=false
 
@@ -59,13 +63,13 @@ done
 source $ENV
 
 # Setup kind multicluster with KubeSlice
-HUB_TEMPLATE="hub.template.yaml"
-SPOKE_TEMPLATE="spoke.template.yaml"
+CONTROLLER_TEMPLATE="controller.template.yaml"
+WORKER_TEMPLATE="worker.template.yaml"
 SLICE_TEMPLATE="slice.template.yaml"
 REGISTRATION_TEMPLATE="clusters-registration.template.yaml"
 
-CLUSTERS=($HUB)
-CLUSTERS+=(${SPOKES[*]})
+CLUSTERS=($CONTROLLER)
+CLUSTERS+=(${WORKERS[*]})
 
 clean() {
     echo Cleaning up all clusters
@@ -133,14 +137,14 @@ fi
 
 # Create kind clusters
 echo Create the Controller cluster
-echo kind create cluster --name $HUB --config hub-cluster.yaml $KIND_K8S_VERSION
-kind create cluster --name $HUB --config hub-cluster.yaml $KIND_K8S_VERSION
+echo kind create cluster --name $CONTROLLER --config controller-cluster.yaml $KIND_K8S_VERSION
+kind create cluster --name $CONTROLLER --config controller-cluster.yaml $KIND_K8S_VERSION
 
 echo Create the Worker clusters
-for CLUSTER in ${SPOKES[@]}; do
+for CLUSTER in ${WORKERS[@]}; do
     echo Creating cluster $CLUSTER
-    echo kind create cluster --name $CLUSTER --config spoke-cluster.yaml $KIND_K8S_VERSION
-    kind create cluster --name $CLUSTER --config spoke-cluster.yaml $KIND_K8S_VERSION
+    echo kind create cluster --name $CLUSTER --config worker-cluster.yaml $KIND_K8S_VERSION
+    kind create cluster --name $CLUSTER --config worker-cluster.yaml $KIND_K8S_VERSION
     # Make sure the cluster context exists
     kubectl cluster-info --context $PREFIX$CLUSTER
 done
@@ -154,12 +158,17 @@ fi
 # Helm repo access
 echo Setting up helm...
 helm repo remove kubeslice
-helm repo add kubeslice  https://kubeslice.github.io/charts/
+if [ "$OSS_CHARTS" == "charts" ]; then
+    helm repo add kubeslice  https://kubeslice.github.io/charts/
+elif [ "$OSS_CHARTS" == "dev-charts" ]; then
+    helm repo add kubeslice https://raw.githubusercontent.com/kubeslice/dev-charts/gh-pages/ --username KRANTHI0918 --password $CR_TOKEN
+fi
+
 helm repo update
 
-# Hub setup...
+# Controller setup...
 echo Switch to controller context and set it up...
-kubectx $PREFIX$HUB
+kubectx $PREFIX$CONTROLLER
 kubectx
 
 helm install cert-manager kubeslice/cert-manager --namespace cert-manager  --create-namespace --set installCRDs=true
@@ -171,19 +180,19 @@ sleep 30
 
 kubectl get pods -n cert-manager
 
-# Install hub-system kubeslice/hub-system
-# First, get the hub's endpoint (sed removes the colors encoded in the cluster-info output)
+# Install kubeslice-controller kubeslice/kubeslice-controller
+# First, get the controller's endpoint (sed removes the colors encoded in the cluster-info output)
 INTERNALIP=`kubectl get nodes -o wide | grep master | awk '{ print $6 }'`
-HUB_ENDPOINT=$INTERNALIP:6443
-echo HUBEndPoint is: $HUB_ENDPOINT
+CONTROLLER_ENDPOINT=$INTERNALIP:6443
+echo CONTROLLEREndPoint is: $CONTROLLER_ENDPOINT
 
-DECODE_HUB_ENDPOINT=`echo -n https://$HUB_ENDPOINT | base64`
-echo Endpoint after base64 is: $DECODE_HUB_ENDPOINT
+DECODE_CONTROLLER_ENDPOINT=`echo -n https://$CONTROLLER_ENDPOINT | base64`
+echo Endpoint after base64 is: $DECODE_CONTROLLER_ENDPOINT
 
-# Make a hub values yaml from the hub template yaml
-HFILE=$HUB-config.yaml
-cp $HUB_TEMPLATE $HFILE
-sed -i "s/ENDPOINT/$HUB_ENDPOINT/g" $HFILE
+# Make a controller values yaml from the controller template yaml
+HFILE=$CONTROLLER-config.yaml
+cp $CONTROLLER_TEMPLATE $HFILE
+sed -i "s/ENDPOINT/$CONTROLLER_ENDPOINT/g" $HFILE
 
 echo "Install the kubeslice-controller"
 helm install kubeslice-controller kubeslice/kubeslice-controller -f controller-config.yaml --namespace kubeslice-controller --create-namespace $CONTROLLER_VERSION
@@ -203,8 +212,8 @@ kubectl apply -f project.yaml -n kubeslice-controller
 REGFILE=clusters-registration.yaml
 cp $REGISTRATION_TEMPLATE $REGFILE
 SCOUNT=1
-for SPOKE in ${SPOKES[@]}; do
-    sed -i "s/SPOKE$SCOUNT/$SPOKE/g" $REGFILE
+for WORKER in ${WORKERS[@]}; do
+    sed -i "s/WORKER$SCOUNT/$WORKER/g" $REGFILE
     SCOUNT=$((SCOUNT+1))
 done
 
@@ -219,18 +228,18 @@ echo kubectl get sa -n kubeslice-avesha
 kubectl get sa -n kubeslice-avesha
 
 
-# Spoke setup
-# Get secret info from hub...
-for SPOKE in ${SPOKES[@]}; do
+# Worker setup
+# Get secret info from controller...
+for WORKER in ${WORKERS[@]}; do
 
-    kubectx $PREFIX$HUB
+    kubectx $PREFIX$CONTROLLER
 
-    SECRET=`kubectl get secrets -n kubeslice-avesha| grep $SPOKE | awk '{print $1}'`
+    SECRET=`kubectl get secrets -n kubeslice-avesha| grep $WORKER | awk '{print $1}'`
     SECRET= `echo -n $SECRET`
-    echo Secret for spoke $SPOKE is: $SECRET
+    echo Secret for worker $WORKER is: $SECRET
 
     # Don't use endpoint from the secrets file... use the one we created above
-    echo "Readable ENDPOINT is: " $DECODE_HUB_ENDPOINT
+    echo "Readable ENDPOINT is: " $DECODE_CONTROLLER_ENDPOINT
 
     NAMESPACE=`kubectl get secrets $SECRET -o yaml -n kubeslice-avesha | grep -m 1 " namespace" | awk '{print $2}'`
     NAMESPACE=`echo -n $NAMESPACE`
@@ -238,7 +247,7 @@ for SPOKE in ${SPOKES[@]}; do
     CACRT=`echo -n $CACRT`
     TOKEN=`kubectl get secrets $SECRET -o yaml -n kubeslice-avesha | grep -m 1 " token" | awk '{print $2}'`
     TOKEN=`echo -n $TOKEN`
-    CLUSTERNAME=`echo -n $SPOKE`
+    CLUSTERNAME=`echo -n $WORKER`
 
     if [ "$VERBOSE" == true ]; then
 	echo Namespace $NAMESPACE
@@ -248,20 +257,20 @@ for SPOKE in ${SPOKES[@]}; do
 	echo ClusterName $CLUSTERNAME
     fi
     
-    # Convert the template info a .yaml for this spoke
-    SFILE=$SPOKE.config.yaml
-    cp $SPOKE_TEMPLATE $SFILE
+    # Convert the template info a .yaml for this worker
+    SFILE=$WORKER.config.yaml
+    cp $WORKER_TEMPLATE $SFILE
     sed -i "s/NAMESPACE/$NAMESPACE/g" $SFILE
-    sed -i "s/ENDPOINT/$DECODE_HUB_ENDPOINT/g" $SFILE
+    sed -i "s/ENDPOINT/$DECODE_CONTROLLER_ENDPOINT/g" $SFILE
     sed -i "s/CACRT/$CACRT/g" $SFILE
     sed -i "s/TOKEN/$TOKEN/g" $SFILE
-    sed -i "s/SPOKENAME/$CLUSTERNAME/g" $SFILE
+    sed -i "s/WORKERNAME/$CLUSTERNAME/g" $SFILE
 
 
-    # Switch to spoke context
-    kubectx $PREFIX$SPOKE
-    SPOKENODEIP=`kubectl get nodes -o wide | grep $SPOKE-worker | head -1 | awk '{ print $6 }'`
-    sed -i "s/NODEIP/$SPOKENODEIP/g" $SFILE
+    # Switch to worker context
+    kubectx $PREFIX$WORKER
+    WORKERNODEIP=`kubectl get nodes -o wide | grep $WORKER-worker | head -1 | awk '{ print $6 }'`
+    sed -i "s/NODEIP/$WORKERNODEIP/g" $SFILE
     helm install kubeslice-worker kubeslice/kubeslice-worker -f $SFILE --namespace kubeslice-system  --create-namespace $WORKER_VERSION
     echo Check for status...
     kubectl get pods -n kubeslice-system
@@ -271,8 +280,8 @@ for SPOKE in ${SPOKES[@]}; do
 done
 
 sleep 60
-echo Switch to hub context and configure slices...
-kubectx $PREFIX$HUB
+echo Switch to controller context and configure slices...
+kubectx $PREFIX$CONTROLLER
 kubectx
 
 # Slice setup
@@ -280,21 +289,21 @@ kubectx
 SFILE=slice.yaml
 cp $SLICE_TEMPLATE $SFILE
 SCOUNT=1
-for SPOKE in ${SPOKES[@]}; do
-    sed -i "s/SPOKE$SCOUNT/$SPOKE/g" $SFILE
+for WORKER in ${WORKERS[@]}; do
+    sed -i "s/WORKER$SCOUNT/$WORKER/g" $SFILE
     SCOUNT=$((SCOUNT+1))
 done
 
 echo kubectl apply -f $SFILE -n kubeslice-avesha
 kubectl apply -f $SFILE -n kubeslice-avesha
 
-echo "Wait for vl3(slice) and gateway pod to be Running in spoke clusters"
+echo "Wait for vl3(slice) and gateway pod to be Running in worker clusters"
 sleep 120
 
 echo "Final status check..."
-for SPOKE in ${SPOKES[@]}; do
-    echo $PREFIX$SPOKE
-    kubectx $PREFIX$SPOKE
+for WORKER in ${WORKERS[@]}; do
+    echo $PREFIX$WORKER
+    kubectx $PREFIX$WORKER
     kubectx
     kubectl get pods -n kubeslice-system
 done
@@ -302,8 +311,8 @@ done
 
 # Iperf setup
 echo Setup Iperf
-# Switch to kind-spoke-1 context
-kubectx $PREFIX${SPOKES[0]}
+# Switch to kind-worker-1 context
+kubectx $PREFIX${WORKERS[0]}
 kubectx
 
 kubectl create ns iperf
@@ -312,8 +321,8 @@ echo "Wait for iperf to be Running"
 sleep 60
 kubectl get pods -n iperf
 
-# Switch to kind-spoke-2 context
-kubectx $PREFIX${SPOKES[1]}
+# Switch to kind-worker-2 context
+kubectx $PREFIX${WORKERS[1]}
 kubectx
 
 kubectl create ns iperf
@@ -322,8 +331,8 @@ echo "Wait for iperf to be Running"
 sleep 60
 kubectl get pods -n iperf
 
-# Switch to spoke context
-kubectx $PREFIX${SPOKES[0]}
+# Switch to worker context
+kubectx $PREFIX${WORKERS[0]}
 kubectx
 
 sleep 90
