ec0f9c26dc854ff68ea9847c187074ff26253cb1
Juan Veras <juan.veras@aveshasystems.com>
2022-06-10 11:10:33 -0400

    Remoded default values from terraform variables. Added new files to gitignore

--- ec2/terraform/variables.tf
+++ ec2/terraform/variables.tf
@@ -1,14 +1,11 @@
 variable "region" {
   type    = string
-  default = "sa-east-1"
 }
 
 variable "key_pair" {
   type    = string
-  default = "kubeslice-ec2"
 }
 
 variable "key_pair_file" {
   type    = string
-  default = "/Users/juanveras/Documents/avesha/github/examples/ec2/ssh_key/kubeslice-ec2.pem"
 }
